02e69ef035f7d9f68cd95cdfd88832a4420df042
Jonathan van Alteren <jvalteren@essentialimagination.com>
2015-02-28 21:45:33 +0100

    Initial commit

---
+++ js/gridGallery.js
@@ -0,0 +1,52 @@
+/**
+ * Excerpt from cbpGridGallery.js v1.0.0
+ * http://www.codrops.com
+ */
+;( function( window ) {
+	
+	'use strict';
+
+	function extend( a, b ) {
+		for( var key in b ) { 
+			if( b.hasOwnProperty( key ) ) {
+				a[key] = b[key];
+			}
+		}
+		return a;
+	}
+
+	function GridGallery( el, options ) {
+		this.el = el;
+		this.options = extend( {}, this.options );
+  		extend( this.options, options );
+  		this._init();
+	}
+
+	GridGallery.prototype.options = {
+	};
+
+	GridGallery.prototype._init = function() {
+		// main grid
+		this.grid = this.el.querySelector( 'section.grid-wrap > ul.grid' );
+		// main grid items
+		this.gridItems = [].slice.call( this.grid.querySelectorAll( 'li:not(.grid-sizer)' ) );
+		// items total
+		this.itemsCount = this.gridItems.length;
+		// init masonry grid
+		this._initMasonry();
+	};
+
+	GridGallery.prototype._initMasonry = function() {
+		var grid = this.grid;
+		imagesLoaded( grid, function() {
+			new Masonry( grid, {
+				itemSelector: 'li',
+				columnWidth: grid.querySelector( '.grid-sizer' )
+			});
+		});
+	};
+
+	// add to global namespace
+	window.GridGallery = GridGallery;
+
+})( window );
